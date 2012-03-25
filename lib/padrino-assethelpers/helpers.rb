eb41300b60b6b1c477710b48db4ca69577af68d0
Ken Erickson <bookworm.productions@gmail.com>
2012-03-24 17:49:33 -0700

    fixes asset_host

--- lib/padrino-assethelpers/helpers.rb
+++ lib/padrino-assethelpers/helpers.rb
@@ -29,38 +29,43 @@ module Padrino
            @@count = 1
         end
         
-        is_absolute  = source =~ %r{^/}
-        asset_folder = asset_folder_name(kind)
-        source = source.to_s.gsub(/\s/, '%20')
-        ignore_extension = (asset_folder.to_s == kind.to_s) # don't append an extension
+        is_absolute      = source =~ %r{^/}
+        asset_folder     = asset_folder_name(kind)
+        source           = source.to_s.gsub(/\s/, '%20')
+        ignore_extension = (asset_folder.to_s == kind.to_s) # don't append an extension       
+        
         source << ".#{kind}" unless (ignore_extension && !(kind == :js or kind == :css) ) or source =~ /\.#{kind}/        
-                
-        if self.class.respond_to?(:assets_host)
-          result_path = rewrite_asset_host(source)              
-        elsif is_absolute  
-          result_path ||= source
-        else 
-          result_path ||= uri_root_path(asset_folder, source)
-        end       
+               
+        if is_absolute
+          result_path = source 
+        else
+          if self.class.respond_to?(:assets_host)     
+            result_path = asset_host(source) 
+            result_path << uri_root_path(asset_folder, source)      
+          else 
+            result_path = uri_root_path(asset_folder, source)      
+          end    
+        end
+
         timestamp = asset_timestamp(result_path, is_absolute)
         
         "#{result_path}#{timestamp}"
       end
 
     private  
-      def uri_root_path(source)
+      def uri_root_path(*paths)
         root_uri = settings.uri_root if settings.respond_to?(:uri_root)
         root_uri = settings.assets_uri_root if settings.respond_to?(:assets_uri_root)
-        File.join(ENV['RACK_BASE_URI'].to_s, root_uri || '/', source)
+        File.join(ENV['RACK_BASE_URI'].to_s, root_uri || '/', *paths)
       end 
          
       def asset_folder_name(kind)
         if kind == :css  
           asset_folder = 'stylesheets'
-          asset_folder = settings.stylesheets_folder if settings.respond_to?(:stylesheets_folder)
+          asset_folder = settings.stylesheets_folder.to_s if settings.respond_to?(:stylesheets_folder)
         elsif kind == :js                               
           asset_folder = 'javascripts'                  
-          asset_folder = settings.javascripts_folder if settings.respond_to?(:javascripts_folder)                
+          asset_folder = settings.javascripts_folder.to_s if settings.respond_to?(:javascripts_folder)                
         else
           asset_folder = kind.to_s
         end 
@@ -68,15 +73,13 @@ module Padrino
         return asset_folder
       end 
 
-
-      def rewrite_asset_host(source)       
+      def asset_host(source)       
         unless settings.assets_host.is_a?(Proc)     
           host = settings.assets_host    
           host = settings.assets_host.gsub("%d", @@count.to_s) if settings.assets_host.index('%d')
         end  
         host = settings.assets_host(source, request) if settings.assets_host.is_a?(Proc)    
-        host ? host + source : source     
-        File.join(host, '/', source)
+        return host
       end       
       
       def asset_host_limit()
